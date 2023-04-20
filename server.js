aae7c3de2494613458a378df4452d0b6ccb9c132
Bishwas Shrestha <bishwas.shrestha@gmail.com>
2023-04-20 16:48:45 +0300

    sever.js updated

--- server.js
+++ server.js
@@ -25,32 +25,6 @@ app.use("/modules", express.static("node_modules"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const httpServer = http.createServer(app);
-
-// // Before routes; otherwise middleware didn't get called
-
-// if (process.env.NODE_ENV === "production") {
-//   app.enable("trust proxy");
-//   app.use((req, res, next) => {
-//     if (req.secure) {
-//       // request was via https, so do no special handling
-//       next();
-//     } else {
-//       // request was via http, so redirect to https
-//       // if express app run under proxy with sub path URL
-//       const proxypath = process.env.PROXY_PASS || "";
-
-//       res.redirect(`https://${req.headers.host}${proxypath}${req.url}`);
-//     }
-//   });
-
-//   app.listen(process.env.PORT);
-// } else {
-//   httpServer.listen(process.env.PORT, () => {
-//     console.log(`app listening on port ${process.env.PORT}`);
-//   });
-// }
-
 app.use("/image", imageRoutes);
 app.use("/auth", authRoutes);
 app.use("/comment", commentRoutes);
